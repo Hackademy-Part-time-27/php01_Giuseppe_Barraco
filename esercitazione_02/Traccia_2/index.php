a4210ef169af4c83fa27c3995bafd925bbfdcc7a
Giuseppe Barraco <beppechef.gb@gmail.com>
2024-03-01 18:47:44 +0100

    correzione sintassi

--- esercitazione_02/Traccia_2/index.php
+++ esercitazione_02/Traccia_2/index.php
@@ -1,17 +1,18 @@
 <?php
 
 $numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
+$count = 0;
 $evens= 0;
 
 foreach($numbers as $value){
     if ($value % 2 == 0) {
         $evens += $value;
+        $count++;
     }else{
         continue;
     }
 }
 
-$media = $evens / 2;
+$media = $evens / $count;
 
 print_r($media);
